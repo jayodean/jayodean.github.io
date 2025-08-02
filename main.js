12e9d28d782382c6ecf55904fea2cc0d8aa741db
jayodean <79191832+jayodean@users.noreply.github.com>
2025-08-02 01:25:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -43,3 +43,13 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 window.scrollToTop = scrollToTop;
+
+const menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+}
+
